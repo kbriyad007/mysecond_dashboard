cd16dd039d3438a7d21a64c6c0b927dad7af74b0
kbriyad007 <kb.riyad007@gmail.com>
2025-06-30 22:42:23 +0600

    page.tsx

--- app/products/page.tsx
+++ app/products/page.tsx
@@ -1,9 +1,18 @@
 import ProductGrid from "./ProductGrid";
+import CartMenu from "../components/CartMenu";
 
 export default function ProductsPage() {
   return (
-    <main className="bg-gray-100 min-h-screen">
-      <ProductGrid />
+    <main className="bg-gray-100 min-h-screen p-6 grid lg:grid-cols-4 gap-6">
+      {/* Product listing on the left (3/4) */}
+      <div className="lg:col-span-3">
+        <ProductGrid />
+      </div>
+
+      {/* Cart menu on the right (1/4) */}
+      <div className="lg:col-span-1">
+        <CartMenu />
+      </div>
     </main>
   );
 }
