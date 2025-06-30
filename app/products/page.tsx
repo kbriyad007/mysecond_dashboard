ae0c1e6a998003da8e8c342f489ddc20cf9e7590
kbriyad007 <kb.riyad007@gmail.com>
2025-06-30 22:04:43 +0600

    page.tsx

---
+++ app/products/page.tsx
@@ -0,0 +1,9 @@
+import ProductGrid from "./ProductGrid";
+
+export default function ProductsPage() {
+  return (
+    <main className="bg-gray-100 min-h-screen">
+      <ProductGrid />
+    </main>
+  );
+}
