6e55cf50aa1f71c21579a4bd3fbaab9019fc3838
kbriyad007 <kb.riyad007@gmail.com>
2025-07-04 02:59:08 +0600

    page.tsx

--- app/products/[slug]/page.tsx
+++ app/products/[slug]/page.tsx
@@ -3,7 +3,6 @@
 import StoryblokClient from 'storyblok-js-client';
 import { notFound } from 'next/navigation';
 
-// Setup client
 const Storyblok = new StoryblokClient({
   accessToken: process.env.NEXT_PUBLIC_STORYBLOK_API_KEY,
 });
@@ -14,15 +13,14 @@ type PageProps = {
 
 export default async function ProductPage({ params }: PageProps) {
   try {
-    // Fetch story by slug
     const { data } = await Storyblok.get(`cdn/stories/products/${params.slug}`, {
-      version: 'draft', // or 'published'
+      version: 'draft',
     });
 
     const productName = data.story.content.product_name;
 
     return <h1>{productName}</h1>;
-  } catch (err) {
+  } catch {
     return notFound();
   }
 }
