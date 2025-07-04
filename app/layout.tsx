507ba39b87b83974414bb17c03e72f36e379c459
kbriyad007 <kb.riyad007@gmail.com>
2025-07-05 00:59:36 +0600

    layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import { CartProvider } from "@/lib/CartContext";
+import { CartProvider } from "@/app/context/CartContext";
 
 // Font setup
 const geistSans = Geist({
